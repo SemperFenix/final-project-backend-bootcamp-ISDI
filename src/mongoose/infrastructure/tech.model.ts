5884853d1c3cd315b7a7ca9f601b412a7585ee22
Ivan Duran <dureivan@gmail.com>
2023-03-13 20:17:22 +0100

    Add tech mongoose model

---
+++ src/mongoose/infrastructure/tech.model.ts
@@ -0,0 +1,52 @@
+import { model, Schema } from 'mongoose';
+import Tech from '../../techniques/domain/techniques';
+
+const techsSchema = new Schema<Tech>({
+  attack: {
+    type: String,
+    required: true,
+  },
+  tech: {
+    type: String,
+    required: true,
+  },
+  stand: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+  usersLearnt: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Aikido_User',
+    },
+  ],
+  usersInProgress: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Aikido_User',
+    },
+  ],
+  usersToLearn: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Aikido_User',
+    },
+  ],
+  video: {
+    type: String,
+  },
+});
+
+techsSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export const TechModel = model('Tech', techsSchema, 'techniques');

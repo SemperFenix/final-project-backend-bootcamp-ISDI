91db097a893572c4a63dc10f8553774dafa74476
Ivan Duran <dureivan@gmail.com>
2023-03-13 18:20:43 +0100

    Add db connection

---
+++ src/db/infrastructure/db.connect.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import { config } from '../../config.js';
+
+const { user, password, cluster, collection } = config;
+
+export const dbConnect = (env?: string) => {
+  const finalEnv = env || process.env.NODE_ENV;
+  const finalDBName =
+    finalEnv === 'test' ? collection + '_Testing' : collection;
+  const uri = `mongodb+srv://${user}:${password}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
+  return mongoose.connect(uri);
+};

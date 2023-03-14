478e8326a1dc75258ef9c75cfad89bb2132538eb
Ivan Duran <dureivan@gmail.com>
2023-03-14 15:33:47 +0100

    Add techs router

---
+++ src/server/infrastructure/routers/techs.router.ts
@@ -0,0 +1,20 @@
+import { Router, Router as router } from 'express';
+import { TechsController } from '../controllers/techs.controllers.js';
+import ServerRouter from '../server.router.js';
+
+export default class TechRouter implements ServerRouter {
+  path: string = '/techniques';
+  router: Router = router();
+
+  // eslint-disable-next-line no-unused-vars
+  constructor(private techsControllers: TechsController) {
+    this.registerControllers();
+  }
+
+  registerControllers(): void {
+    this.router.post(
+      '/add',
+      this.techsControllers.create.bind(this.techsControllers)
+    );
+  }
+}

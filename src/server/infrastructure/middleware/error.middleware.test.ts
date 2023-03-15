3800bdda7387e32314d4e80494f0b0180d254533
SemperFenix <dureivan@gmail.com>
2023-03-15 10:32:48 +0100

    Add error middleware test

---
+++ src/server/infrastructure/middleware/error.middleware.test.ts
@@ -0,0 +1,25 @@
+import { CustomError } from '../../../common/errors/http.error';
+import { mockNext, mockReq, mockRes } from '../../../common/mocks/test.mocks';
+import { errorMiddleware } from './error.middleware';
+
+describe('Given the errorMidleware function', () => {
+  describe('When called with all values in custom Error', () => {
+    test('Then it should call resp.json with values in the argument', () => {
+      const error = { code: 200, outMsg: 'test' } as CustomError;
+
+      errorMiddleware(error, mockReq, mockRes, mockNext);
+      expect(mockRes.status).toHaveBeenCalledWith(error.code);
+      expect(mockRes.json).toHaveBeenCalled();
+    });
+  });
+
+  describe('When called without values in custom error', () => {
+    test('Then it should call resp.json with values in the argument', () => {
+      const error = {} as CustomError;
+
+      errorMiddleware(error, mockReq, mockRes, mockNext);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.json).toHaveBeenCalled();
+    });
+  });
+});

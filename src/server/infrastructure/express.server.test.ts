310322a9cd03e2ea9ab1b7596986ee3813f55880
SemperFenix <dureivan@gmail.com>
2023-03-15 10:22:12 +0100

    Add express server test

--- src/server/infrastructure/express.server.test.ts
+++ src/server/infrastructure/express.server.test.ts
@@ -5,33 +5,37 @@ import http from 'http';
 import { dbConnect } from './mongoose/db/db.connect';
 
 // Comentado para continuar con ello
-// jest.mock('./mongoose/db/db.connect', () => ({
-//   dbConnect: jest.fn(() =>
-//     Promise.resolve(() => ({
-//       db: jest.fn().mockImplementation(() => ({
-//         databaseName: 'testing',
-//       })),
-//     }))
-//   ),
-// }));
+jest.mock('./mongoose/db/db.connect', () => ({
+  dbConnect: jest.fn(() =>
+    Promise.resolve({
+      connection: {
+        db: {
+          databaseName: 'test',
+        },
+      },
+    })
+  ),
+}));
 
 const router = new AikidoUserRouter(mockAikidoUsersController);
 const server = new ExpressServer([router]);
 const spy = jest.spyOn(http, 'createServer');
 
 describe('Given the ExpressServer class', () => {
-  beforeEach(() => {});
   describe('When instantiated', () => {
     test('Then it should create an instance', () => {});
 
     expect(server).toBeInstanceOf(ExpressServer);
   });
   describe('When a calling its start method', () => {
-    test('Then it should call http create server and dbconnect', () => {
-      server.start(3040);
+    test('Then it should call http create server and dbconnect', async () => {
+      const result = server.start(3040);
+
       expect(spy).toHaveBeenCalled();
-      // Comentado para continuarlo
-      // expect(dbConnect).toHaveBeenCalled();
+      expect(dbConnect).toHaveBeenCalled();
+      result.emit('listening', ['test']);
+      result.close();
+      result.unref();
     });
   });
 });

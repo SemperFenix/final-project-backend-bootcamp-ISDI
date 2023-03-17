b44c305395f02aaaa7bfb93976aa23657632c852
SemperFenix <dureivan@gmail.com>
2023-03-17 12:23:52 +0100

    Add use case test

---
+++ src/aikido.users/application/aikido.users.searcherPaged.test.ts
@@ -0,0 +1,30 @@
+import { mockAikidoUserRepo } from '../../common/mocks/test.mocks.js';
+import AikidoUserSearcherPaged from './aikido.users.searcherPaged.js';
+
+describe('Given the AikidoUserSearcherPaged class', () => {
+  describe('When call its execute method', () => {
+    test('Then it should call the update repo method', async () => {
+      const aikidoUserSearcherPaged = new AikidoUserSearcherPaged(
+        mockAikidoUserRepo
+      );
+      await aikidoUserSearcherPaged.execute(
+        [
+          {
+            key: 'Test',
+            value: 'TestVal',
+          },
+        ],
+        0
+      );
+      expect(mockAikidoUserRepo.searchPaged).toHaveBeenCalledWith(
+        [
+          {
+            key: 'Test',
+            value: 'TestVal',
+          },
+        ],
+        0
+      );
+    });
+  });
+});

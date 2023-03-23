7448aef9e33d68450cd8b1cc5bc71797e078c92c
SemperFenix <dureivan@gmail.com>
2023-03-23 13:02:42 +0100

    Add techs routes to router

--- src/server/infrastructure/routers/techs.router.ts
+++ src/server/infrastructure/routers/techs.router.ts
@@ -1,5 +1,6 @@
 import { Router, Router as router } from 'express';
 import { TechsController } from '../../application/controllers/techs.controllers.js';
+import { Interceptors } from '../middleware/interceptors.middleware.js';
 import ServerRouter from '../server.router.js';
 
 export default class TechRouter implements ServerRouter {
@@ -14,7 +15,34 @@ export default class TechRouter implements ServerRouter {
   registerControllers(): void {
     this.router.post(
       '/add',
+      Interceptors.logged,
+      Interceptors.admin,
       this.techsControllers.create.bind(this.techsControllers)
+    );
+
+    this.router.get(
+      '/:id',
+      Interceptors.logged,
+      this.techsControllers.queryById.bind(this.techsControllers)
+    );
+
+    this.router.patch(
+      '/update/:id',
+      Interceptors.logged,
+      Interceptors.admin,
+      this.techsControllers.update.bind(this.techsControllers)
+    );
+
+    this.router.get(
+      '/list/filter?:id',
+      Interceptors.logged,
+      this.techsControllers.queryCategorized.bind(this.techsControllers)
+    );
+
+    this.router.get(
+      '/list/:id?:id',
+      Interceptors.logged,
+      this.techsControllers.queryAll.bind(this.techsControllers)
     );
   }
 }
